5cb566168efecf24ee9120175d969e40be3f4638
naokiyamaguchi <naokiyamaguchi@gmail.com>
2021-05-05 13:11:45 +0900

    project first commit

--- category/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import Category
-
-# コンテキストプロセッサーを使うことでviewsで指定することなくデフォルトでtemplateに組み込むことができる
-
-
-def menu_request(request):
-    links = Category.objects.all()
-    return dict(links=links)
